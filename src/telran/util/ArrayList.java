afa7adc5a8afecf6dfc74262f986d23946e18614
Kirillgutsalo <kirya.sever11@gmail.com>
2021-11-08 12:54:20 +0200

    initial commit

---
+++ src/telran/util/ArrayList.java
@@ -0,0 +1,143 @@
+package telran.util;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class ArrayList<T> implements List<T> {
+	private static final int DEFAULT_CAPACITY = 16;
+	private T[] array;
+	private int size = 0; 
+	@SuppressWarnings("unchecked")
+	public ArrayList(int capacity) {
+		array = (T[]) new Object[capacity];
+	}
+	public ArrayList() {
+		this(DEFAULT_CAPACITY);
+	}
+	@Override
+	public void add(T element) {
+		//O[1]
+		if (size == array.length) {
+			//size is capacity
+			allocate();
+		}
+		array[size++] = element;
+		
+		
+	}
+	
+		
+	private void allocate() {
+		array = Arrays.copyOf(array, array.length * 2);
+		
+	}
+	@Override
+	public boolean add(int index, T element) {
+		//O[N]
+		boolean res = false;
+		if (index == size) {
+			add(element);
+			res = true;
+			
+		} else if(isValidIndex(index)) {
+			res = true;
+			if (size == array.length) {
+				allocate();
+			}
+			System.arraycopy(array, index, array, index + 1, size - index);
+			array[index] = element;
+			size++;
+		}
+		return res;
+	}
+
+	@Override
+	public int size() {
+		//O[1]
+		return size;
+	}
+
+	@Override
+	public T get(int index) {
+		//O[1]
+		return isValidIndex(index) ? array[index] : null;
+	}
+
+	private boolean isValidIndex(int index) {
+		
+		return index >= 0 && index < size;
+	}
+	@Override
+	public T remove(int index) {
+		//O[N]
+		T res = null;
+		if (isValidIndex(index)) {
+			res = array[index];
+			size--;
+			System.arraycopy(array, index + 1, array, index, size - index);
+			//FIXME regarding setting null
+		}
+		
+		return res;
+	}
+	
+	
+	
+	
+	@Override
+	public int indexOf(Predicate<T> predicate) {
+		//O[N]
+				int res = -1;
+				for (int i = 0; i < size; i++) {
+					if (predicate.test(array[i])) {
+						res = i;
+						break;
+					}
+				}
+				return res;
+	}
+	@Override
+	public int lastIndexOf(Predicate<T> predicate) {
+		//O[N]
+				int res = -1;
+				for (int i = size - 1; i >=0 ; i--) {
+					if (predicate.test(array[i])) {
+						res = i;
+						break;
+					}
+				}
+				return res;
+	}
+	@Override
+	public boolean removeIf(Predicate<T> predicate) {
+		//O[N^2]
+		int oldSize = size;
+		for (int i = size - 1; i >= 0; i--) {
+			if (predicate.test(array[i])) {
+				remove(i);
+			}
+		}
+		
+		return oldSize > size;
+		//TODO rewrite the method for O[N] complexity
+	}
+	@Override
+	public void sort(Comparator<T> comp) {
+		//O[N * LogN]
+		Arrays.sort(array, 0, size, comp);
+		
+	}
+	@Override
+	public int sortedSearch(T pattern, Comparator<T> comp) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
